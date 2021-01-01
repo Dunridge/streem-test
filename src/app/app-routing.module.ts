70fe49fddfbec976161a75bf83de96a7e13123ee
Dunridge <Polinka7max@gmail.com>
2021-01-01 18:20:14 +0200

    fixed routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,17 +2,28 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {UsersComponent} from './components/users/users.component';
 import {AppComponent} from './app.component';
+import {PostComponent} from './components/post/post.component';
 
+// TODO: try adding the users route as a child route to the default route
 const routes: Routes = [
-  {
-    path: '',
-    component: AppComponent,
-    pathMatch: 'full',
-  },
+  // {
+  //   path: '',
+  //   redirectTo: 'users',
+  //   pathMatch: 'full'
+  // },
   {
     path: 'users',
     component: UsersComponent
   },
+  {
+    path: 'users/:userId',
+    component: PostComponent
+  },
+  {
+    path: '',
+    component: AppComponent,
+    pathMatch: 'full',
+  }
 ];
 
 @NgModule({
